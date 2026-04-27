5fe5346f46dc6e4784ac21259ba0cc7b13852878
Gabriel Savoy <bielsavoy2020@gmail.com>
2026-04-27 10:53:47 -0300

    Implementa menu com switch-case

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+float calcularMedia();
+void classificarAluno(float media);
+void derivadaSimples();
+
 int main() {
     int opcao;
 
@@ -10,6 +14,12 @@ int main() {
     printf("0 - Sair\n");
 
     scanf("%d", &opcao);
+
+    switch(opcao) {
+        case 1: calcularMedia(); break;
+        case 2: classificarAluno(0); break;
+        case 3: derivadaSimples(); break;
+    }
 
     return 0;
 }

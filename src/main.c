a47aba0c076800dc533f338113ae8c6ee9822c96
Gabriel Savoy <bielsavoy2020@gmail.com>
2026-04-27 10:50:32 -0300

    Adiciona menu inicial do sistema

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    
+    int opcao;
 
-    
+    printf("===== SISTEMA =====\n");
+    printf("1 - Media\n");
+    printf("2 - Classificar\n");
+    printf("3 - Derivada\n");
+    printf("0 - Sair\n");
+
+    scanf("%d", &opcao);
+
     return 0;
 }

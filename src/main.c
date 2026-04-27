f2bcb8b932f00fc865a7dcefc9698d5bede09c55
victorvidigalxz <victor.v.inacio@gmail.com>
2026-04-27 11:46:32 -0300

    Adiciona entrada dos coeficientes

--- src/main.c
+++ src/main.c
@@ -91,3 +91,10 @@ void classificarAluno(float media) {
     else
         printf("Reprovado\n");
 }
+void derivadaSimples() {
+    int a, b, c;
+
+    printf("a: "); scanf("%d", &a);
+    printf("b: "); scanf("%d", &b);
+    printf("c: "); scanf("%d", &c);
+}
